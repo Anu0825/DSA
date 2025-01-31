c57af4fb31f3984150480a84cc3c1f9fe9730398
Anu025 <anug26464@gmail.com>
2025-01-31 16:25:39 +0530

    flag

--- searching_and_sorting/quick_sort.cpp
+++ searching_and_sorting/quick_sort.cpp
@@ -1,52 +1,54 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int partition(int arr[],int s,int e){
-    int pivot=arr[s];
-    int count=0;
-    for(int i=s+1;i<=e;i++){
-        if(arr[i]<=pivot)
-             count++;
-    }
-    // place pivot at its right position
-    int pivotIndex=s+count;
-    swap(arr[pivotIndex],arr[s]);
-    //left and right vala part
-    int i=s,j=e;
-    while(i<pivotIndex && j>pivotIndex){
-        while(arr[i]<=pivot){
+
+int partition(vector<int> &arr, int low, int high) {
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
+
+    while (i < j) {
+        while (arr[i] <= pivot && i <= high - 1) {
             i++;
         }
-        while(arr[j]>pivot){
+
+        while (arr[j] > pivot && j >= low + 1) {
             j--;
         }
-        if(i<pivotIndex && j>pivotIndex){
-            swap(arr[i++],arr[j--]);
-        }
+        if (i < j) swap(arr[i], arr[j]);
     }
-    return pivotIndex;
+    swap(arr[low], arr[j]);
+    return j;
 }
-void quick_sort(int arr[],int s,int e){
-    if(s>=e)
-        return;
-    //partition ke liye
-    int p=partition(arr,s,e);
-    //left part sort ke liye
-    quick_sort(arr,s,p-1);
-    //right part sort ke liye
-    quick_sort(arr,p+1,e);
+
+void qs(vector<int> &arr, int low, int high) {
+    if (low < high) {
+        int pIndex = partition(arr, low, high);
+        qs(arr, low, pIndex - 1);
+        qs(arr, pIndex + 1, high);
+    }
 }
-int main(){
-    int n;
-    cout<<"enter n:";
-    cin>>n;
-    int arr[n];
-    cout<<"enter array elements:"<<endl;
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+
+vector<int> quickSort(vector<int> arr) {
+    qs(arr, 0, arr.size() - 1);
+    return arr;
+}
+
+int main()
+{
+    vector<int> arr = {4, 6, 2, 5, 7, 9, 1, 3};
+    int n = arr.size();
+    cout << "Before Using quick Sort: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
     }
-    quick_sort(arr,0,n-1);
-    cout<<"sorted array is : ";
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
+    cout << endl;
+
+    arr = quickSort(arr);
+    cout << "After Using quick sort: " << "\n";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
     }
+    cout << "\n";
+    return 0;
 }
